608f6f485e029c3a4fcb3a21d1b31d49a09f19dc
gowtham-sathyan <gowthamsathyan@gmail.com>
2021-01-08 03:00:16 +0400

    Added class to reduce execution time

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import requests
 import os
 import re
 import time
-import stanza
-from extract_text import extract
-# stanza.download('en')
+from extract_text import extraction
 app = Flask(__name__)
 cors = CORS(app)
 
@@ -16,10 +14,11 @@ def get_results():
 	f=request.files['file']
 	filename=str(time.time())+".pdf"
 	f.save(filename)
-	payload=extract(filename)
+	payload=pdf_extractor.extract(filename)
 	# pdf_text=extract_text(filename)
 	return(jsonify(payload))
 
+pdf_extractor=extraction()
 if __name__=='__main__':
 	# app.run(host='0.0.0.0',port=5000,debug=True)
 	app.run(debug=True)
--- extract_text.py
+++ extract_text.py
@@ -1,119 +1,124 @@
 from pdfminer.high_level import extract_text
 import re
 import stanza
-def extract(filename):
-	nlp = stanza.Pipeline('en',processors='tokenize,ner,pos')
-	pdf_text=extract_text(filename)
-	text_1=re.sub("\s\s\s+" , "\n", pdf_text)
-	text_1=text_1.split("\n")
-	del pdf_text
-	text=[]
-	i=0
-	while i<len(text_1):
-		status_flag=0
-		j=text_1[i].strip()
-		if j=="" or j==" ":
-			text_1.remove(text_1[i])
-		else:
-			while status_flag==0:
-				ind=len(j)-1
-				while j[ind]==" ":
-					ind=ind-1
-				if j[ind]!="," or i==len(text_1)-1:
-					text.append(j)
-					i+=1
-					status_flag=1
-				else:
-					j+=text_1[i+1].strip()
-					i+=1
-	# t_list=["gpe", "org"]
-	# skipped=[]
-	del text_1
-	result=set()
-	org_flag=0
-	loc_flag=0
-	for i in text:
-		doc=nlp(i)
-		doc_list=doc.to_dict()
-		wanted_flag=0
-		date_flag=0
-		unwanted_counter=0
-		dob_counter=0
-		num_counter=0
-		for j in range(len(doc_list)):
-			for k in doc_list[j]:
-				try:
-					if k["upos"]!="PUNCT":
-						if "gpe" not in k["ner"].lower():
-							loc_flag=0
-						if "date" in k["ner"].lower():
-							if k["ner"]=="S-DATE":
-								pass
-							elif k["ner"]=="B-DATE":
-								dob_counter+=1
-								if k["upos"]=="NUM":
-									num_counter+=1
-								elif k["upos"]!="PUNCT":
-									month_flag=k["upos"]
-							elif k["ner"]=="I-DATE" and dob_counter==1:
-								dob_counter+=1
-								if k["upos"]=="NUM":
-									num_counter+=1
-								elif k["upos"]!="PUNCT":
-									month_flag=k["upos"]
-							elif k["ner"]=="E-DATE" and dob_counter==2:
-								if k["upos"]=="NUM":
-									num_counter+=1
-								elif k["upos"]!="PUNCT":
-									month_flag=k["upos"]
-								if (num_counter==2 and month_flag=="PROPN"):
-									dob_counter=0
-									num_counter=0
+
+class extraction:
+
+	def __init__(self):
+		self.nlp = stanza.Pipeline('en',processors='tokenize,ner,pos')
+	def extract(self,filename):
+		# nlp = stanza.Pipeline('en',processors='tokenize,ner,pos')
+		pdf_text=extract_text(filename)
+		text_1=re.sub("\s\s\s+" , "\n", pdf_text)
+		text_1=text_1.split("\n")
+		del pdf_text
+		text=[]
+		i=0
+		while i<len(text_1):
+			status_flag=0
+			j=text_1[i].strip()
+			if j=="" or j==" ":
+				text_1.remove(text_1[i])
+			else:
+				while status_flag==0:
+					ind=len(j)-1
+					while j[ind]==" ":
+						ind=ind-1
+					if j[ind]!="," or i==len(text_1)-1:
+						text.append(j)
+						i+=1
+						status_flag=1
+					else:
+						j+=text_1[i+1].strip()
+						i+=1
+		# t_list=["gpe", "org"]
+		# skipped=[]
+		del text_1
+		result=set()
+		org_flag=0
+		loc_flag=0
+		for i in text:
+			doc=self.nlp(i)
+			doc_list=doc.to_dict()
+			wanted_flag=0
+			date_flag=0
+			unwanted_counter=0
+			dob_counter=0
+			num_counter=0
+			for j in range(len(doc_list)):
+				for k in doc_list[j]:
+					try:
+						if k["upos"]!="PUNCT":
+							if "gpe" not in k["ner"].lower():
+								loc_flag=0
+							if "date" in k["ner"].lower():
+								if k["ner"]=="S-DATE":
+									pass
+								elif k["ner"]=="B-DATE":
+									dob_counter+=1
+									if k["upos"]=="NUM":
+										num_counter+=1
+									elif k["upos"]!="PUNCT":
+										month_flag=k["upos"]
+								elif k["ner"]=="I-DATE" and dob_counter==1:
+									dob_counter+=1
+									if k["upos"]=="NUM":
+										num_counter+=1
+									elif k["upos"]!="PUNCT":
+										month_flag=k["upos"]
+								elif k["ner"]=="E-DATE" and dob_counter==2:
+									if k["upos"]=="NUM":
+										num_counter+=1
+									elif k["upos"]!="PUNCT":
+										month_flag=k["upos"]
+									if (num_counter==2 and month_flag=="PROPN"):
+										dob_counter=0
+										num_counter=0
+									else:
+										result.add(doc.sentences[j].text)
+										date_flag=1
+										break
 								else:
 									result.add(doc.sentences[j].text)
 									date_flag=1
 									break
+							elif "org" in k["ner"].lower():
+								wanted_flag=1
+								org_flag=1
+							elif "gpe" in k["ner"].lower() and (org_flag==1 or loc_flag==1):
+								wanted_flag=1
+								org_flag=0
+								loc_flag=1
 							else:
-								result.add(doc.sentences[j].text)
-								date_flag=1
-								break
-						elif "org" in k["ner"].lower():
-							wanted_flag=1
-							org_flag=1
-						elif "gpe" in k["ner"].lower() and (org_flag==1 or loc_flag==1):
-							wanted_flag=1
-							org_flag=0
-							loc_flag=1
-						else:
-							unwanted_counter+=1
-				except:
-					pass
-		if date_flag!=1 and wanted_flag==1:
-			if unwanted_counter<=3:
-				result.add(i)
-	del nlp
-	result_dict={}
-	for i in result:
-		try:
-			j=text.index[i]
-			try:
-				result_dict[text.index(i)].append(i)
-				result_dict[text.index(i)]=sorted(result_dict[text.index(i)], key=lambda x: text.index(i).index(x))
-			except:
-				result_dict[text.index(i)]=[i]
-		except:
-			for j in range(len(text)):
-				if i in text[j]:
-					try:
-						result_dict[j].append(i)
-						result_dict[j]=sorted(result_dict[j], key=lambda x: text[j].index(x))
+								unwanted_counter+=1
 					except:
-						result_dict[j]=[i]
-					break
-	del text
-	del result
-	payload={"result":[]}
-	for i in sorted(result_dict.keys()):
-		for j in result_dict[i]:
-			payload["result"].append(j)
-	return payload
+						pass
+			if date_flag!=1 and wanted_flag==1:
+				if unwanted_counter<=3:
+					result.add(i)
+		del nlp
+		result_dict={}
+		for i in result:
+			try:
+				j=text.index[i]
+				try:
+					result_dict[text.index(i)].append(i)
+					result_dict[text.index(i)]=sorted(result_dict[text.index(i)], key=lambda x: text.index(i).index(x))
+				except:
+					result_dict[text.index(i)]=[i]
+			except:
+				for j in range(len(text)):
+					if i in text[j]:
+						try:
+							result_dict[j].append(i)
+							result_dict[j]=sorted(result_dict[j], key=lambda x: text[j].index(x))
+						except:
+							result_dict[j]=[i]
+						break
+		del text
+		del result
+		payload={"result":[]}
+		for i in sorted(result_dict.keys()):
+			for j in result_dict[i]:
+				payload["result"].append(j)
+		return payload
